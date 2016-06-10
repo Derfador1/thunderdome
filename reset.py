3e22c8dce8c3233974d07d425c43b7e5bfb0c3a7
Derfador1 <Derfador1@gmail.com>
2016-06-10 15:58:05 -0400

    changed to work the correct way

--- reset.py
+++ reset.py
@@ -1,11 +1,25 @@
 #! /usr/bin/python3
 
 import psycopg2
+import sys
 
 def main():
-	conn = psycopg.connect("dbname='sparadis'")
+	arg1 = sys.argv[1]
+		
+	conn = psycopg2.connect(database=arg1)
 	cursor = conn.cursor()
-	cursor.execute("delete from fight")
+	cursor.execute('delete from fight *')
+	"""
+	cursor.execute("drop table attack cascade;")
+	cursor.execute("drop table combatant cascade;")
+	cursor.execute("drop table fight cascade;")
+	cursor.execute("drop table species cascade;")
+	cursor.execute("drop table species_attack cascade;")
+	cursor.execute("drop table type cascade;")
+	"""
+	
+	conn.commit()
+	conn.close()
 
 if __name__ == "__main__":
 	main()
