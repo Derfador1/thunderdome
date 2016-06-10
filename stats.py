a93e932776246706156355ca8617d7be2372dadf
Derfador1 <Derfador1@gmail.com>
2016-06-09 20:14:56 -0400

    commiting current copy but not able to push

---
+++ stats.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+import random
+import sys
+import psycopg2
+
+def print_menu():
+	menu = [
+		'1. Longest fight?',
+		'2. Shortest fight?', '3. Won most fights?',
+		'4. Lost most fights', '5. Most possible attacks',
+		'6. Win/Loss/Draw table', '7. Exit'
+		]
+
+	for item in menu:
+		print(item)	
+		
+		#clear results all need to do is delete from table_name
+
+def main():
+	list1 = []
+	
+	conn = psycopg2.connect("dbname='sparadis'")
+	
+	cursor = conn.cursor()
+	
+	cursor.execute('select * from combatant')
+	
+	rows = cursor.fetchall()
+
+	for row in rows:
+		print(row)
+	
+	print_menu()
+	
+	conn.close()
+
+if __name__ == "__main__":
+	main()
