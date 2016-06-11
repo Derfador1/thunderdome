094308621c4f7ef961961f230f529c28ec4dc29d
Derfador1 <Derfador1@gmail.com>
2016-06-11 13:02:55 -0400

    commented the main body of code

--- stats.py
+++ stats.py
@@ -39,6 +39,7 @@ def main():
 		
 		choice = input("Choice:")
 
+		#grab all info from fight table
 		try:
 			cur.execute("select * from fight")
 		except Exception as e:
@@ -46,7 +47,8 @@ def main():
 			exit(1)
 			
 		fightfacts = cur.fetchall()
-
+		
+		#grab all info from combatant table
 		try:
 			cur.execute("select * from combatant")
 		except Exception as e:
@@ -66,6 +68,7 @@ def main():
 		if ( choice < '3' ):
 			#this query and setup style was found through the help of
 			#https://github.com/JMcLaurin/thunderdome/blob/master/stats.py
+			#this query joins to tables and sets up info for time
 			sql_query = "SELECT id, name, SUM(finish-start)"
 			sql_query += "FROM combatant JOIN fight ON id=combatant_one OR"
 			sql_query += " id=combatant_two GROUP BY id ORDER BY sum DESC;"
@@ -73,6 +76,7 @@ def main():
 			cur.execute(sql_query) 
 			time = cur.fetchall()			
 
+			#simple prints for both 1 and 2 saying shortest and longest time
 			if choice == '1':
 				print(time[0][1], "fought for", time[0][2])
 			elif choice == '2':
@@ -80,12 +84,15 @@ def main():
 		elif ( choice == '3' ):
 			highestnumskills = [0, 0]
 			for each in combatantinfo:
+				#for each combatant from there possible attacks
 				cur.execute("select * from species_attack where species_id = " + str(each[0]))
 				skilllist = cur.fetchall()
+				#checks for greatest length as it steps through
 				if (len(skilllist) > highestnumskills[0]):
 					highestnumskills[0] = each[0]
 					highestnumskills[1] = len(skilllist)
 
+			#print out the one/ones witht he highest skill number
 			for each in combatantinfo:
 				if each[0] == highestnumskills[0]:
 					print(each[1], "has the most skills available at",
@@ -97,6 +104,7 @@ def main():
 				ListOfThings.append(0)
 
 			if (choice == '4'):
+				#add up the most wins out of the fighters
 				for x in range(1, len(combatantinfo)):
 					for thing in fightfacts:
 						if ((int(thing[0]) == x) and (thing[2] == "One") or
@@ -104,6 +112,7 @@ def main():
 							ListOfThings[x]+=1
 				print("The most wins was: ",max(ListOfThings))
 			elif (choice == '5'):
+				#add up the most losses out of the fighters
 				for x in range(1, len(combatantinfo)):
 					for thing in fightfacts:
 						if ((int(thing[1]) == x) and (thing[2] == "One")
@@ -111,6 +120,7 @@ def main():
 							ListOfThings[x]+=1
 				print("The most Losses was: ",max(ListOfThings))
 			elif (choice == '6'):
+				#add up the draws out of the fighters
 				for x in range(1, len(combatantinfo)):
 					for thing in fightfacts:
 						if ((int(thing[0]) == x) and (thing[2] == "Tie")
@@ -120,22 +130,7 @@ def main():
 		if(choice == '7'):
 			print('quitting..')
 			sys.exit()
-
-	"""
-	while(1):
-		user_input = input('Choice: ')
-
-		if(user_input == '1'):
-			print("here")
-			longest_fight = longest(cursor)
-			print(longest_fight)
-		elif(user_input == '2'):
-			print('here 2')
-			shortest_fight = shortest(cursor)
-			print(shortest_fight)
-		else:
-			break
-	"""
+			
 	conn.close()
 
 if __name__ == "__main__":
